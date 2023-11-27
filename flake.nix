64cd698f3254e83fab175d22b06045966ee2fe4e
Gary Guo <gary.guo@lowrisc.org>
2023-11-27 14:50:09 +0000

    Initial flake
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    all_system_outputs = flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      formatter = pkgs.alejandra;
+    });
+  in
+    all_system_outputs;
+}
