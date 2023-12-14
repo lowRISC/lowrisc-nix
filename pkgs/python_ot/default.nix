cdaa08a70aaf9dfb3cbe8e519ebcb6fc37695486
Hugo McNally <hugo.mcnally@gmail.com>
2023-12-14 15:57:46 +0000

    Added License
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

--- pkgs/python_ot/default.nix
+++ pkgs/python_ot/default.nix
@@ -1,3 +1,7 @@
+# Copyright lowRISC Contributors.
+# Licensed under both the MIT License and the Apache License, Version 2.
+# See LICENSE-MIT and LICENSE-APACHE for details.
+# SPDX-License-Identifier: MIT OR Apache-2.0
 {
   inputs,
   pkgs,
