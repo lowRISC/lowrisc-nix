3751ce8def5a57900b27c07cb0f9ca3a2898c07c
Hugo McNally <hugo.mcnally@gmail.com>
2023-12-19 14:03:21 +0000

    Removed Apache license
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 # Copyright lowRISC Contributors.
-# Licensed under both the MIT License and the Apache License, Version 2.
-# See LICENSE-MIT and LICENSE-APACHE for details.
-# SPDX-License-Identifier: MIT OR Apache-2.0
+# Licensed under the MIT License, see LICENSE for details.
+# SPDX-License-Identifier: MIT
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
--- pkgs/python_ot/sitecustomize.py
+++ pkgs/python_ot/sitecustomize.py
@@ -1,8 +1,7 @@
 # Copyright lowRISC Contributors
 # Copyright Nixpkgs/NixOS Contributors
-# Licensed under both the MIT License and the Apache License, Version 2.
-# See LICENSE-MIT and LICENSE-APACHE for details.
-# SPDX-License-Identifier: MIT OR Apache-2.0
+# Licensed under the MIT License, see LICENSE for details.
+# SPDX-License-Identifier: MIT
 
 """
 This is a Nix-specific module for discovering modules built with Nix.
