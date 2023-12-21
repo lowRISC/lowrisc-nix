8953f1d9f2662c1bc8107acd02276fc273ae9ab7
Gary Guo <gary.guo@lowrisc.org>
2023-12-21 11:24:30 +0000

    Add lowrisc-toolchain-gcc-rv32imcb package
    Co-authored-by: Harry Callahan <hcallahan@lowrisc.org>
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,11 @@
   pkgs,
   inputs,
   ...
-}: {
+}:
+{
   verilator_ot = import ./verilator {inherit pkgs;};
   python_ot = pkgs.callPackage ./python_ot {inherit inputs;};
+}
+// pkgs.lib.optionalAttrs (pkgs.system == "x86_64-linux") {
+  lowrisc-toolchain-gcc-rv32imcb = pkgs.callPackage ./lowrisc-toolchain-gcc-rv32imcb.nix {};
 }
---
+++ pkgs/lowrisc-toolchain-gcc-rv32imcb.nix
@@ -0,0 +1,31 @@
+{
+  fetchzip,
+  stdenv,
+  lib,
+  ncurses5,
+}:
+stdenv.mkDerivation rec {
+  name = "lowrisc-toolchain-gcc-rv32imcb";
+  version = "20230427-1";
+  src = fetchzip {
+    url = "https://github.com/lowRISC/lowrisc-toolchains/releases/download/${version}/lowrisc-toolchain-gcc-rv32imcb-${version}.tar.xz";
+    sha256 = "sha256-DwecY4WUqenpRHUIjWdybhOVJwzT173HO7S02KQcpb4=";
+  };
+
+  dontConfigure = true;
+  dontBuild = true;
+  dontPatchELF = true; # We will do this manually in preFixup
+  dontStrip = true;
+
+  installPhase = ''
+    mkdir -p $out
+    cp -R * $out
+  '';
+  preFixup = ''
+    find $out -type f ! -name ".o" | while read f; do
+      patchelf "$f" > /dev/null 2>&1                                                  || continue
+      patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) "$f"  || true
+      patchelf --set-rpath ${lib.makeLibraryPath ["$out" stdenv.cc.cc ncurses5]} "$f" || true
+    done
+  '';
+}
