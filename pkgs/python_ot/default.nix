5fbdd394b55e8f773b670ed870b1511835722929
Gary Guo <gary.guo@lowrisc.org>
2024-01-02 14:54:43 +0000

    Move poetryOverrides to a separate file and export it

--- pkgs/python_ot/default.nix
+++ pkgs/python_ot/default.nix
@@ -8,72 +8,12 @@
   ...
 }: let
   poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix {inherit pkgs;};
-
-  # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md
-  # poetry2nix tries to build the python packages based on the information
-  # given in their own build description files (setup.py etc.)
-  # Sometimes, the inputs are incomplete. Add missing inputs here.
-  pypkgs-missing-build-requirements = {
-    # package: build-requirements #
-    attrs = ["hatchling" "hatch-fancy-pypi-readme" "hatch-vcs"];
-    beautifulsoup4 = ["hatchling"];
-    pyfinite = ["setuptools"];
-    zipfile2 = ["setuptools"];
-    okonomiyaki = ["setuptools"];
-    simplesat = ["setuptools"];
-    urllib3 = ["hatchling"];
-    fusesoc = ["setuptools" "setuptools-scm"];
-    chipwhisperer = ["setuptools"];
-  };
-  buildreqs-overlay = (
-    final: prev:
-      builtins.mapAttrs (
-        package: build-requirements:
-          (builtins.getAttr package prev).overridePythonAttrs (old: {
-            buildInputs =
-              (old.buildInputs or [])
-              ++ (
-                builtins.map
-                (pkg: builtins.getAttr pkg final)
-                build-requirements
-              );
-          })
-      )
-      pypkgs-missing-build-requirements
-  );
-
-  # The following modules are very slow to build or are otherwise broken.
-  # For now, preferWheel to pull the binary dist.
-  preferwheel-overlay = final: prev: {
-    mypy = prev.mypy.override {
-      # Very slow build.
-      preferWheel = true;
-    };
-    libcst = prev.libcst.override {
-      # Missing rustc/cargo etc.
-      # buildInputs = (old.buildInputs or []) ++ [ final.setuptools-rust ];
-      preferWheel = true;
-    };
-    isort = prev.isort.override {
-      # Some problem building due to a malformed semantic version string.
-      preferWheel = true;
-    };
-  };
-
-  # Nix wraps python programs which causes PATH to change. We want to keep
-  # PATH in case a program needs to invoke user-defined programs.
-  dontwrap-overlay = final: prev: {
-    fusesoc = prev.fusesoc.overridePythonAttrs {
-      dontWrapPythonPrograms = true;
-    };
-  };
+  poetryOverrides = inputs.self.poetryOverrides {inherit pkgs;};
 in
   poetry2nix.mkPoetryEnv {
     projectDir = ./.;
     overrides = [
-      preferwheel-overlay
-      buildreqs-overlay
-      dontwrap-overlay
+      poetryOverrides
       poetry2nix.defaultPoetryOverrides
     ];
   }
