2c41ce12dd9d37e4268731c49ff1f58ad197165a
Gary Guo <gary.guo@lowrisc.org>
2024-01-03 14:18:22 +0000

    Add a dev shell for opentitan

--- flake.nix
+++ flake.nix
@@ -26,8 +26,13 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-    in {
+    in rec {
       packages = import ./pkgs {inherit pkgs inputs;};
+      devShells = {
+        opentitan = pkgs.callPackage ./dev/opentitan.nix {
+          inherit (packages) ncurses5-fhs verilator_ot python_ot;
+        };
+      };
       formatter = pkgs.alejandra;
     });
   in
