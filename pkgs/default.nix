bd3e77c409105628872be528848f4f328bf8178a
Gary Guo <gary.guo@lowrisc.org>
2024-01-03 17:39:46 +0000

    Add ncurse5-fhs package for libtinfo.so.5 in FHS

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,6 +7,8 @@
   ...
 }:
 {
+  ncurses5-fhs = pkgs.callPackage ./ncurses5-fhs.nix {};
+
   verilator_ot = import ./verilator {inherit pkgs;};
   python_ot = pkgs.callPackage ./python_ot {inherit inputs;};
 }
---
+++ pkgs/ncurses5-fhs.nix
@@ -0,0 +1,19 @@
+{
+  runCommand,
+  ncurses5,
+  patchelf,
+  ...
+}:
+# If SONAME is specified, lookup cache files created by ldconfig will only use the SONAME and will ignore
+# the filename, this casues libtinfo.so to not be found under FHS. Patch ncurses5 to provide a libtinfo.so
+# with proper SONAME.
+runCommand "ncurses5" {
+  outputs = ["out" "dev" "man"];
+} ''
+  cp -r ${ncurses5} $out
+  chmod +w $out/lib
+  cp -L --no-preserve=mode --remove-destination `realpath $out/lib/libtinfo.so.5` $out/lib/libtinfo.so.5
+  ${patchelf}/bin/patchelf --set-soname libtinfo.so.5 $out/lib/libtinfo.so.5
+  cp -r ${ncurses5.dev} $dev
+  cp -r ${ncurses5.man} $man
+''
