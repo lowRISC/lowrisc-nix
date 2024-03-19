148ed4b7ec48a783aebaa1c551f66b3b6eb9c1a5
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-03-19 11:18:18 +0000

    Add Verible package
    This uses the version we're currently requiring in OpenTitan
    tool_dependencies.py

---
+++ pkgs/verible.nix
@@ -0,0 +1,35 @@
+# Copyright lowRISC contributors.
+#
+# SPDX-License-Identifier: MIT
+{
+  pkgs,
+  lib,
+  fetchFromGitHub,
+  verible,
+  bazel_6,
+}:
+verible.override (prev: {
+  buildBazelPackage = args:
+    prev.buildBazelPackage (args
+      // rec {
+        GIT_DATE = "2024-03-13";
+        GIT_VERSION = "v0.0-3622-g07b310a3";
+
+        version = builtins.concatStringsSep "." (lib.take 3 (lib.drop 1 (builtins.splitVersion GIT_VERSION)));
+
+        src = fetchFromGitHub {
+          owner = "chipsalliance";
+          repo = "verible";
+          rev = GIT_VERSION;
+          sha256 = "sha256-pfFb2tuT7F7SpGMXElsVAHxO6xGQrVC15EyYxPSKaK8=";
+        };
+
+        fetchAttrs = {
+          sha256 = "sha256-mLW7gg0+6W/Lt1/Egg0RTFcHvy+DzlHJ3gGOl4D65jE=";
+        };
+
+        patches = [];
+
+        bazel = bazel_6;
+      });
+})
