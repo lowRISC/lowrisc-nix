fc6804431209e51fc542b24cf776a876aea24935
Gary Guo <gary.guo@lowrisc.org>
2024-04-11 14:29:56 +0100

    Run user's current shell instead of bash

--- dev/opentitan.nix
+++ dev/opentitan.nix
@@ -82,5 +82,7 @@ in
       "--symlink ${pkgs.openssl.out}/etc/ssl/openssl.cnf /etc/ssl/openssl.cnf"
       "--symlink /etc/ssl/certs/ca-certificates.crt /etc/ssl/cert.pem"
     ];
+
+    runScript = "\${SHELL:-bash}";
   })
   .env
