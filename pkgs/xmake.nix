1eb956ea74341dee862944478e06f2dc4a9b215d
Gary Guo <gary.guo@lowrisc.org>
2024-05-20 14:56:06 +0100

    Use vendored tbox in xmake
    This allows xmake to build on MacOS

--- pkgs/xmake.nix
+++ pkgs/xmake.nix
@@ -3,7 +3,7 @@
 {pkgs}:
 pkgs.xmake.overrideAttrs (prev: {
   # Remove the cjson lua package from the dependancies.
-  buildInputs = builtins.filter (input: input != pkgs.lua.pkgs.cjson) prev.buildInputs;
+  buildInputs = builtins.filter (input: !(builtins.elem input [pkgs.lua.pkgs.cjson pkgs.tbox])) prev.buildInputs;
   # Reset the configure flags so they don't contain `external=y`.
   configureFlags = [];
   enableParallelBuilding = true;
