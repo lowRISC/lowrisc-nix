ef39da1f7a502f18706255e7cf68b0e423de8d52
Gary Guo <gary.guo@lowrisc.org>
2024-09-25 17:22:36 +0100

    Remove uf2conv Python propagated input

--- pkgs/uf2conv.nix
+++ pkgs/uf2conv.nix
@@ -4,20 +4,32 @@
 {
   python3Packages,
   fetchFromGitHub,
+  stdenv,
   lib,
-}:
-python3Packages.buildPythonPackage rec {
-  name = "uf2conv";
-  version = "0.0.1";
-  src = fetchFromGitHub {
-    owner = "makerdiary";
-    repo = "uf2utils";
-    rev = "61d9f3ff4913461b9813e666cce62b022652144a";
-    hash = "sha256-3mXOvzkHmTV0k8qoy2k5TGBFX2l9EgPuaZPLyTdBHzQ=";
+}: let
+  app = python3Packages.buildPythonApplication rec {
+    name = "uf2conv";
+    version = "0.0.1";
+    src = fetchFromGitHub {
+      owner = "makerdiary";
+      repo = "uf2utils";
+      rev = "61d9f3ff4913461b9813e666cce62b022652144a";
+      hash = "sha256-3mXOvzkHmTV0k8qoy2k5TGBFX2l9EgPuaZPLyTdBHzQ=";
+    };
+    meta = {
+      description = "uf2conv is an open source Python based tool for packing and unpacking UF2 files.";
+      homepage = "https://github.com/makerdiary/uf2utils";
+      license = lib.licenses.mit;
+    };
   };
-  meta = {
-    description = "uf2conv is an open source Python based tool for packing and unpacking UF2 files.";
-    homepage = "https://github.com/makerdiary/uf2utils";
-    license = lib.licenses.mit;
-  };
-}
+in
+  # `buildPythonApplication` sets `propagatedBuildInputs` which in turn pulls Python into path
+  # when used in `mkShell`. Launder it through custom derivation to remove it.
+  stdenv.mkDerivation {
+    inherit (app) name version meta;
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/bin
+      ln -s ${app}/bin/uf2conv $out/bin
+    '';
+  }
