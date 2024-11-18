de6d3c955b15474391938c3f17c8c7ca9933e357
Gary Guo <gary.guo@lowrisc.org>
2024-11-18 16:22:19 +0000

    Add caliptra dev environment

---
+++ dev/caliptra.nix
@@ -0,0 +1,16 @@
+# Copyright lowRISC contributors.
+#
+# SPDX-License-Identifier: MIT
+{
+  pkgs,
+  verilator_caliptra,
+  riscv64-gcc_caliptra,
+}:
+pkgs.mkShell {
+  name = "caliptra";
+
+  nativeBuildInputs = with pkgs; [
+    verilator_caliptra
+    riscv64-gcc_caliptra
+  ];
+}
