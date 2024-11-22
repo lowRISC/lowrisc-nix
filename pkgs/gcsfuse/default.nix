caf96668d2cb25c12dc761a58d5c0802a5bd5474
Gary Guo <gary.guo@lowrisc.org>
2024-11-22 21:39:02 +0000

    gcsfuse: 2.4.0 -> 2.5.1

--- pkgs/gcsfuse/default.nix
+++ pkgs/gcsfuse/default.nix
@@ -2,23 +2,26 @@
 # Licensed under the MIT License, see LICENSE for details.
 # SPDX-License-Identifier: MIT
 {
+  stdenv,
+  go_1_23,
   gcsfuse,
   fetchFromGitHub,
 }:
 gcsfuse.override (prev: {
   buildGoModule = args:
-    prev.buildGoModule (args
+    (prev.buildGoModule.override {go = go_1_23;}) (args
       // rec {
-        version = "2.4.0";
+        # This is the upstream version, but we pin it.
+        version = "2.5.1";
 
         src = fetchFromGitHub {
           owner = "googlecloudplatform";
           repo = "gcsfuse";
           rev = "v${version}";
-          hash = "sha256-4susiXFe1aBcakxRkhmOe7dvcwsNfam4KKyFFzYXhcU=";
+          hash = "sha256-4UzRg6fNKBrTSoimJ9jURW9oPRhUOAUDMG3JaM7f100=";
         };
 
-        vendorHash = "sha256-uOr929RS8q7LB+WDiyxEIyScE/brmvPJKfnq28PfsDM=";
+        vendorHash = "sha256-QrpILFzgUQwmrvjCdtrlgq1zSW7f82qMHsifI39WaB0=";
 
         patches = [
           # https://github.com/GoogleCloudPlatform/gcsfuse/pull/2269
@@ -26,5 +29,12 @@ gcsfuse.override (prev: {
           # https://github.com/GoogleCloudPlatform/gcsfuse/pull/2285
           ./symlink-cache.patch
         ];
+
+        meta =
+          args.meta
+          // {
+            mainProgram = "gcsfuse";
+            broken = stdenv.hostPlatform.isDarwin;
+          };
       });
 })
