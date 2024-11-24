5743c3cac33b6536771e9aade767e504886f5638
Gary Guo <gary.guo@lowrisc.org>
2024-11-24 20:35:50 +0000

    verilator_caliptra: pin to systemc 2.3.4

--- pkgs/verilator_caliptra.nix
+++ pkgs/verilator_caliptra.nix
@@ -2,18 +2,28 @@
 # Licensed under the MIT License, see LICENSE for details.
 # SPDX-License-Identifier: MIT
 {
-  pkgs,
   systemc,
   gcc12Stdenv,
+  fetchFromGitHub,
+  verilator,
 }:
-(pkgs.verilator.override {
+(verilator.override {
   # Long symbols are broken when compiled with GCC 13: https://github.com/verilator/verilator/issues/4204
   stdenv = gcc12Stdenv;
-  systemc = systemc.override {stdenv = gcc12Stdenv;};
+  systemc = (systemc.override {stdenv = gcc12Stdenv;}).overrideAttrs rec {
+    version = "2.3.4";
+
+    src = fetchFromGitHub {
+      owner = "accellera-official";
+      repo = "systemc";
+      rev = version;
+      sha256 = "0sj8wlkp68cjhmkd9c9lvm3lk3sckczpz7w9vby64inc1f9fnf0b";
+    };
+  };
 })
 .overrideAttrs rec {
   version = "5.012";
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "verilator";
     repo = "verilator";
     rev = "v${version}";
