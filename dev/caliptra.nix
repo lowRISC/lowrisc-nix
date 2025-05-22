769c88c32a27d9847ca53429f014174a5f34a5a2
Elliot Baptist <elliot.baptist@lowrisc.org>
2025-05-22 09:48:11 +0100

    dev/caliptra: add pyyaml package to python3
    Needed for run_verilator_l0_regression.py script.

--- dev/caliptra.nix
+++ dev/caliptra.nix
@@ -5,13 +5,22 @@
   pkgs,
   verilator_caliptra,
   riscv64-gcc_caliptra,
-}:
-pkgs.mkShell {
-  name = "caliptra";
+  python3,
+}: let
+  pyEnv = python3.withPackages (
+    ps:
+      with ps; [
+        pyyaml
+      ]
+  );
+in
+  pkgs.mkShell {
+    name = "caliptra";
 
-  nativeBuildInputs = with pkgs; [
-    verilator_caliptra
-    riscv64-gcc_caliptra
-    hjson
-  ];
-}
+    nativeBuildInputs = with pkgs; [
+      verilator_caliptra
+      riscv64-gcc_caliptra
+      hjson
+      pyEnv
+    ];
+  }
