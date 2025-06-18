f83655ebd92574f0b9556b2b672bb1e19056df20
Harry Callahan <hcallahan@lowrisc.org>
2025-06-18 15:30:09 +0100

    Generate output attributes for multiple python sets

--- pkgs/python_ot/default.nix
+++ pkgs/python_ot/default.nix
@@ -8,31 +8,49 @@
   lib,
   ...
 }: let
-  workspace = inputs.uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
-  overlay = workspace.mkPyprojectOverlay {
-    sourcePreference = "sdist";
+  pythonSets = {
+    "main" = python310;
+    "earlgrey_1.0.0" = python310;
   };
 
-  pythonSet =
-    (pkgs.callPackage inputs.pyproject-nix.build.packages {
-      python = python310;
-    })
-    .overrideScope
-    (
-      lib.composeManyExtensions [
-        inputs.pyproject-build-systems.overlays.default
-        overlay
-        (inputs.self.lib.pyprojectOverrides {inherit pkgs;})
-      ]
-    );
+  mkEnv = workspaceRoot: python: let
+    workspace = inputs.uv2nix.lib.workspace.loadWorkspace {
+      workspaceRoot = ./. + "/${workspaceRoot}";
+    };
+    overlay = workspace.mkPyprojectOverlay {
+      sourcePreference = "sdist";
+    };
 
-  env = pythonSet.mkVirtualEnv "opentitan-env" workspace.deps.default;
-in
-  env
-  // {
-    meta =
-      env.meta
+    pythonSet =
+      (pkgs.callPackage inputs.pyproject-nix.build.packages {
+        inherit python;
+      })
+          .overrideScope
+      (
+        lib.composeManyExtensions [
+          inputs.pyproject-build-systems.overlays.default
+          overlay
+          (inputs.self.lib.pyprojectOverrides {inherit pkgs;})
+        ]
+      );
+
+    env = pythonSet.mkVirtualEnv "opentitan-env" workspace.deps.default;
+  in
+    env;
+in let
+  sets = lib.genAttrs (builtins.attrNames pythonSets) (
+    set: let
+      env = mkEnv set (pythonSets.${set});
+    in
+      env
       // {
-        platforms = lib.platforms.linux;
-      };
-  }
+        meta =
+          env.meta
+          // {
+            platforms = lib.platforms.linux;
+          };
+      }
+  );
+in
+  sets."main"
+  // sets
