68b346e50ab12ec0c8a63c67f647981ef8620f47
Harry Callahan <hcallahan@lowrisc.org>
2025-06-19 17:15:25 +0100

    Change soname for libncurses

--- pkgs/ncurses6-fhs.nix
+++ pkgs/ncurses6-fhs.nix
@@ -9,8 +9,8 @@
   ...
 }:
 # If SONAME is specified, lookup cache files created by ldconfig will only use the SONAME and will ignore
-# the filename, this casues libtinfo.so to not be found under FHS. Patch ncurses5 to provide a libtinfo.so
-# with proper SONAME.
+# the filename. This can prevent libtinfo.so and libncurses.so from being found under FHS.
+# Patch the ncurses package to provide libs with the correct SONAMEs.
 runCommand "ncurses6" {
   outputs = ["out" "dev" "man"];
   meta.platforms = lib.platforms.linux;
@@ -19,6 +19,8 @@ runCommand "ncurses6" {
   chmod +w $out/lib
   cp -L --no-preserve=mode --remove-destination `realpath $out/lib/libtinfo.so.6` $out/lib/libtinfo.so.6
   ${patchelf}/bin/patchelf --set-soname libtinfo.so.6 $out/lib/libtinfo.so.6
+  cp -L --no-preserve=mode --remove-destination `realpath $out/lib/libncurses.so.6` $out/lib/libncurses.so.6
+  ${patchelf}/bin/patchelf --set-soname libncurses.so.6 $out/lib/libncurses.so.6
   cp -r ${ncurses6.dev} $dev
   cp -r ${ncurses6.man} $man
 ''
