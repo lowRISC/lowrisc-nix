9348b6a2c67d3a3b563ae4ff9b704e6cde6057fa
Gary Guo <gary.guo@lowrisc.org>
2025-07-17 14:41:40 +0100

    Move flake inputs that are not intended for override to separate flake.nix

---
+++ private/flake.nix
@@ -0,0 +1,28 @@
+# Copyright lowRISC Contributors.
+#
+# SPDX-License-Identifier: MIT
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+    };
+  };
+
+  outputs = _: {};
+}
