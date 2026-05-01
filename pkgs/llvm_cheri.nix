5fdf4909cd49e3ffa425c2c2313a1cb76c9150d0
Douglas Reis <doreis@lowrisc.org>
2026-05-01 14:04:33 +0100

    cheri-llvm: Bump to version 19.0.0
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- pkgs/llvm_cheri.nix
+++ pkgs/llvm_cheri.nix
@@ -2,39 +2,70 @@
 #
 # SPDX-License-Identifier: MIT
 {
-  fetchzip,
-  stdenv,
+  lib,
+  fetchFromGitHub,
+  cmake,
+  ninja,
+  python3,
   zlib,
-  zstd,
-  ncurses6,
+  lld,
+  stdenv,
   autoPatchelfHook,
 }:
 stdenv.mkDerivation rec {
-  name = "cheri-llvm";
-  version = "17.0.1.r02";
-  src = fetchzip {
-    # We build the toolchain in another repository because derivations doesn't allow internet
-    # connection, which is required by cheribuild.
-    url = "https://github.com/engdoreis/cheribuild/releases/download/v${version}/cheri-std093-sdk.tar.gz";
-    hash = "sha256-SJsi9PYSi8UMRbHaLeR88AociKSZL1fdbOWhcqp3bE4=";
+  pname = "llvm-cheri";
+  version = "19.0.0";
+
+  src = fetchFromGitHub {
+    owner = "qwattash";
+    repo = "cheri-alliance-llvm-project";
+    rev = "7dc859cb98e10b0e8dec4fed62185cc543ff31cd";
+    fetchSubmodules = true;
+    hash = "sha256-91GI7YtigLEund7zjbSktbroFhla713CBHH10Bh6/a8=";
   };
-  dontConfigure = true;
-  dontBuild = true;
+
+  sourceRoot = "${src.name}/llvm";
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    lld
+    python3
+    autoPatchelfHook
+  ];
 
   buildInputs = [
-    stdenv.cc.cc.lib
-    ncurses6
     zlib
-    zstd
   ];
-  nativeBuildInputs = [autoPatchelfHook];
 
-  installPhase = ''
-    mkdir -p $out
-    cp -R ./cheri-std093-sdk/* $out
+  preBuild = ''
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [zlib stdenv.cc.cc.lib]}:$LD_LIBRARY_PATH
   '';
 
-  meta = {
-    platforms = ["x86_64-linux"];
-  };
+  cmakeFlags = [
+    "-DLLVM_ENABLE_PROJECTS=llvm;clang;lld;clang-tools-extra"
+    "-DLLVM_TARGETS_TO_BUILD=RISCV;host"
+
+    "-DLLVM_ENABLE_ZLIB=FORCE_ON"
+    "-DLLVM_ENABLE_LIBXML2=FALSE"
+    "-DLLVM_ENABLE_OCAMLDOC=FALSE"
+    "-DLLVM_ENABLE_BINDINGS=FALSE"
+    "-DLLVM_INCLUDE_EXAMPLES=FALSE"
+    "-DLLVM_INCLUDE_DOCS=FALSE"
+    "-DLLVM_INCLUDE_BENCHMARKS=FALSE"
+    "-DLLVM_INSTALL_UTILS=TRUE"
+    "-DLLVM_INSTALL_BINUTILS_SYMLINKS=TRUE"
+    "-DCLANG_ENABLE_STATIC_ANALYZER=FALSE"
+    "-DCLANG_ENABLE_ARCMT=FALSE"
+    "-DLLVM_ENABLE_Z3_SOLVER=FALSE"
+    "-DLLVM_TOOL_LLVM_MCA_BUILD=FALSE"
+    "-DLLVM_TOOL_LLVM_EXEGESIS_BUILD=FALSE"
+    "-DLLVM_TOOL_LLVM_RC_BUILD=FALSE"
+    "-DLLVM_ENABLE_LLD=TRUE"
+    "-DLLVM_OPTIMIZED_TABLEGEN=FALSE"
+    "-DLLVM_USE_SPLIT_DWARF=TRUE"
+    "-DLLVM_ENABLE_ASSERTIONS=TRUE"
+    "-DCLANG_ROUND_TRIP_CC1_ARGS=FALSE"
+    "-DLLVM_PARALLEL_LINK_JOBS=4"
+  ];
 }
